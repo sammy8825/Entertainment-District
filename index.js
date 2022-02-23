e6c58ac32471d89bd4623fa87e09fe6d6ad737c4
sammy8825 <sb25112001@gmail.com>
2022-02-23 15:12:01 +0530

    Added Landing Page

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const app = express();
 
 const port = 8800;
 
+app.use('/images', express.static('images'));
+app.use('/css', express.static('css'));
+
+// For Bootstrap
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('landing', {message: 'This is the message passed from the index.js file'});
+    res.render('landing');
 });
 
 app.listen(port, () => {
