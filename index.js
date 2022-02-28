c9ec5d93c7daef2bbf0b960f18944eb167343127
sammy8825 <sb25112001@gmail.com>
2022-03-01 02:36:17 +0530

    Added Login page

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-const express = require('express');
-const path = require('path');
-const ejs = require('ejs');
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const app = express();
 
 const port = 8800;
 
-app.use('/images', express.static('images'));
-app.use('/css', express.static('css'));
+app.use("/images", express.static("images"));
+app.use("/css", express.static("css"));
 
 // For Bootstrap
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('landing');
+app.get("/", (req, res) => {
+  res.render("landing", { authenticated: false });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", urlencodedParser, (req, res) => {
+  const creds = { email: req.body.email, password: req.body.password };
+  const users = { email: "sb25112001@gmail.com", password: "Sammy@2511" };
+  console.log(creds);
+
+  if (creds.email === users.email && creds.password === users.password)
+    res.render("landing", { authenticated: true });
+  else res.send("Opps!!! Wrong credentials");
 });
 
 app.listen(port, () => {
-    console.log(`Listening to post ${port}`);
+  console.log(`Listening to post ${port}`);
 });
