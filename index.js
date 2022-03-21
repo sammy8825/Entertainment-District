94a4b083b27327e32ffe2baf3db96e57d9ee4cc5
sammy8825 <sb25112001@gmail.com>
2022-03-22 00:37:18 +0530

    Added all the functionalities

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const path = require("path");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const req = require("express/lib/request");
+const axios = require("axios").default;
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const User = require("./js/userSchemaFile");
-const { createBrotliDecompress } = require("zlib");
 const app = express();
 const port = 8800;
 
-let creds;
+let creds, cred;
 
 app.use("/images", express.static("images"));
 app.use("/css", express.static("css"));
@@ -80,7 +79,7 @@ app.post("/signUp", urlencodedParser, (req, res) => {
             if (err) {
               console.log(err);
             } else {
-              let cred = {
+              cred = {
                 username: data.username,
                 email: data.email,
               };
@@ -97,20 +96,7 @@ app.post("/signUp", urlencodedParser, (req, res) => {
 
 // Directing to the home page
 app.get("/home", (req, res) => {
-  User.findOne(
-    { username: creds.username },
-    (err, data) => {
-      if (err) {
-        console.log(err);
-      } else {
-        let cred = {
-          username: data.username,
-          email: data.email,
-        };
-        res.render("home", { cred });
-      }
-    }
-  );
+  res.render("home", { cred });
 });
 
 // Checking the login credentials and redirecting accordingly
@@ -132,7 +118,7 @@ app.post("/login", urlencodedParser, (req, res) => {
             if (err) {
               console.log(err);
             } else {
-              let cred = {
+              cred = {
                 username: data.username,
                 email: data.email,
               };
@@ -149,19 +135,29 @@ app.post("/login", urlencodedParser, (req, res) => {
 
 //Directing to the user profile page
 app.get("/profile", (req, res) => {
-  User.find({ loggedIn: true }, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let cred = {
-        username: data[0].username,
-        email: data[0].email,
-      };
-      res.render("profile", { cred });
-    }
-  });
+  res.render("profile", { cred });
 });
 
+// For Changing the password of the website
+app.post("/profile", urlencodedParser, (req, res) => {
+  if (req.body.newPass === req.body.confNewPass) {
+    User.findOneAndUpdate(
+      { username: creds.username },
+      { password: req.body.newPass, loggedIn: false },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("login", {
+            message: "Your password was changed successfully!!!",
+          });
+        }
+      }
+    );
+  }
+});
+
+// Logging out of the website 
 app.get("/logout", (req, res) => {
   User.findOneAndUpdate(
     { username: creds.username },
@@ -176,8 +172,130 @@ app.get("/logout", (req, res) => {
   );
 });
 
-// Connecting to the database
 
+// Directing to Movie Page
+app.get("/movie", (req, res) => {
+  let movieData = {
+    name: "Title",
+    year: "Year",
+    rated: "Rated",
+    runTime: "Runtime",
+    genre: "Genre",
+    director: "Director",
+    poster: "/images/movies.jpg",
+    rating: "0.0",
+    plot: "Plot",
+  };
+  res.render("movie", { cred, movieData, message: "" });
+});
+
+app.post("/movie", urlencodedParser, (req, res) => {
+  let movieName = req.body.movieName;
+  let options = {
+    method: "GET",
+    url: "https://imdb-data-searching.p.rapidapi.com/om",
+    params: { t: movieName },
+    headers: {
+      "x-rapidapi-host": "imdb-data-searching.p.rapidapi.com",
+      "x-rapidapi-key": "29369dc035msha2998b688575b98p18c8fbjsncd0930f90cac",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      if (response.data.Type === "movie") {
+        // console.log(response.data);
+        let movieData = {
+          name: response.data.Title,
+          year: response.data.Year,
+          rated: response.data.Rated,
+          runTime: response.data.Runtime,
+          genre: response.data.Genre,
+          director: response.data.Director,
+          poster: response.data.Poster,
+          rating: response.data.imdbRating,
+          plot: response.data.Plot,
+        };
+        res.render("movie", {
+          cred,
+          movieData,
+          message: "",
+        });
+      } else {
+        res.render("movie", {
+          cred,
+          message: "No such movie found!!!",
+        });
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+// Directing to Series Page
+app.get("/series", (req, res) => {
+  let seriesData = {
+    name: "Title",
+    year: "Year",
+    rated: "Rated",
+    runTime: "Runtime",
+    genre: "Genre",
+    director: "Director",
+    poster: "/images/series.jpg",
+    rating: "0.0",
+    plot: "Plot",
+  };
+  res.render("series", { cred, seriesData, message: "" });
+});
+
+app.post("/series", urlencodedParser, (req, res) => {
+  let seriesName = req.body.seriesName;
+  let options = {
+    method: "GET",
+    url: "https://imdb-data-searching.p.rapidapi.com/om",
+    params: { t: seriesName },
+    headers: {
+      "x-rapidapi-host": "imdb-data-searching.p.rapidapi.com",
+      "x-rapidapi-key": "29369dc035msha2998b688575b98p18c8fbjsncd0930f90cac",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      if (response.data.Type === "series") {
+        // console.log(response.data);
+        let seriesData = {
+          name: response.data.Title,
+          year: response.data.Year,
+          rated: response.data.Rated,
+          runTime: response.data.Runtime,
+          genre: response.data.Genre,
+          director: response.data.Director,
+          poster: response.data.Poster,
+          rating: response.data.imdbRating,
+          plot: response.data.Plot,
+        };
+        res.render("series", {
+          cred,
+          seriesData,
+          message: "",
+        });
+      } else {
+        res.render("series", {
+          cred,
+          message: "No such series found!!!",
+        });
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+// Connecting to the database
 mongoose.connect(
   "mongodb://localhost/mydb",
   () => {
